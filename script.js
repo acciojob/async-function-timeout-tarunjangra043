9b13a9aca38c94e60df502d3e110b8569b176e02
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-22 20:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async function() {
+  const text = document.getElementById("text").value;
+  const delay = parseInt(document.getElementById("delay").value);
+
+  function delayPromise(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  await delayPromise(delay);
+
+  document.getElementById("output").innerText = text;
+});
